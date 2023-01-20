d6cf83189fe4d6edeff3b3b2c0239b27274f2a37
Masih <masih.saldanha@gmail.com>
2023-01-20 14:04:04 -0300

    feat: add route DELETE '/{id}'

--- src/main/java/com/crudlover/api/controller/CarsController.java
+++ src/main/java/com/crudlover/api/controller/CarsController.java
@@ -3,8 +3,11 @@ package com.crudlover.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +32,11 @@ public class CarsController {
     @PostMapping
     public void create(@RequestBody @Valid CarsDTO req) {
         repository.save(new Cars(req));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 
     @PostMapping("/show")
